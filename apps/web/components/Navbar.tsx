a0688f56fd946dac7d3408ef43e53a5c9a66ba2c
ronnieli <lihinfei0506@gmail.com>
2022-02-22 22:08:23 +0800

    add navbar

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,22 +1,44 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useUser } from '../lib/context';
 
 function Navbar() {
+  const { user } = useUser();
   const router = useRouter();
   return (
-    <div>
-      <div>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/login">
-          <a>Login</a>
-        </Link>
-        <Link href="/register">
-          <a>Register</a>
-        </Link>
-      </div>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <button>Recipe App</button>
+          </Link>
+        </li>
+        {user?.firstName && user?.lastName && (
+          <>
+            <li>
+              <Link href="/">
+                <button>Sign Out</button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin">
+                <button>Write recipe</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${user.firstName}`}></Link>
+            </li>
+          </>
+        )}
+        {!user.firstName && (
+          <li>
+            <Link href="/login">
+              <button>Login</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 }
 
