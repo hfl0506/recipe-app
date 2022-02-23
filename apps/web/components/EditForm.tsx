37489c93d77866da5a275ebbbeb8457e7751fcd0
ronnieli <lihinfei0506@gmail.com>
2022-02-23 21:49:34 +0800

    finished form set up

--- apps/web/components/EditForm.tsx
+++ apps/web/components/EditForm.tsx
@@ -1,5 +1,67 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 function EditForm() {
-  return <div>editform</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    password: '',
+    password2: '',
+  });
+  const { name, password, password2 } = formData;
+
+  const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onUpdate = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      //TODO pass formData = redux call
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onUpdate}>
+        <div>
+          <label>username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={name}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label>password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label>confirm password</label>
+          <input
+            type="password"
+            name="password2"
+            id="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default EditForm;
--- apps/web/components/UploadForm.tsx
+++ apps/web/components/UploadForm.tsx
@@ -1,21 +1,59 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
 function UploadForm() {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    imageUrl: '',
+  });
+  const { title, description, imageUrl } = formData;
+
+  const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+    } catch (error) {}
+  };
   return (
     <section>
       <form>
         <div>
           <label htmlFor="">title</label>
-          <input />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={onChange}
+          />
         </div>
         <div>
           <label htmlFor="">description</label>
-          <input />
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={onChange}
+          />
         </div>
         <div>
           <label htmlFor="">image</label>
-          <input />
+          <input
+            type="file"
+            name="imageUrl"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={onChange}
+          />
         </div>
         <div>
-          <button>upload</button>
+          <button type="submit">upload</button>
         </div>
       </form>
     </section>
